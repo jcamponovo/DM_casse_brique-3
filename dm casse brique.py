9df435cc98d99224671bc8a993dab5e573f056a4
Nathandidr <117632468+Nathandidr@users.noreply.github.com>
2022-12-04 14:58:07 +0100

    Update dm casse brique.py

--- dm casse brique.py
+++ dm casse brique.py
@@ -5,10 +5,13 @@ import pyxel, random
 health_points = 3
 score = 0
 
+
+
 # screen's size
 screen_width = 400
 screen_height = 300
 pyxel.init(screen_width, screen_height)
+
 
 
 # variables liées à la plateforme
@@ -21,6 +24,7 @@ platform_downHeight = 2
 platform_altitude = 10
 platform_x = (screen_width/2) - (platform_width/2)
 platform_y = screen_height - platform_height - platform_altitude
+
 
 
 # variables liées à la balle
@@ -36,6 +40,7 @@ ball_acceleration_x = - 1
 ball_acceleration_y = - 1
 
 
+
 # coordonnées des murs
 wall_Right = screen_width
 wall_Left = 0
@@ -44,14 +49,16 @@ wall_Up = 0
 
 
 
-# variables liées aux briques
+# variables liées aux briques:
+
 # variables liées à l'apparence des briques
 brick_width = 20
 brick_height = 10
 brick_color = 10
 brick_color_2 = 3
 brick_color_3 = 8
-#variables des coordonnées en x des briques
+
+# variables des coordonnées en x des briques
 b_x1 = 70
 b_x2 = 110
 b_x3 = 150
@@ -59,11 +66,13 @@ b_x4 = 190
 b_x5 = 230
 b_x6 = 270
 b_x7 = 310
-#variables des coordonnées en y des briques
+
+# variables des coordonnées en y des briques
 b_y1 = 50
 b_y2 = 75
 b_y3 = 100
-#listes des coordonnées en x et y des briques
+
+# listes des coordonnées en x et y des briques
 bricks_x = [b_x1, b_x2, b_x3, b_x4, b_x5, b_x6, b_x7]
 bricks_y = [b_y1, b_y2, b_y3]
 bricks_niveau1 = [brick_color, brick_color, brick_color, brick_color, brick_color, brick_color, brick_color, brick_color, brick_color_2, brick_color_3, brick_color_3, brick_color_3, brick_color_2, brick_color, brick_color, brick_color, brick_color, brick_color, brick_color, brick_color, brick_color]  
@@ -71,10 +80,14 @@ bricks_niveau2 = [brick_color_2, brick_color, brick_color, brick_color, brick_co
 bricks_niveau3 = [brick_color_3, brick_color_3, brick_color_3, brick_color_3, brick_color_3, brick_color_3, brick_color_3, brick_color_3, brick_color_2, brick_color_2, brick_color_2, brick_color_2, brick_color_2, brick_color_3, brick_color_3, brick_color_3, brick_color_3, brick_color_3, brick_color_3, brick_color_3, brick_color_3]
 rang = 0
 bricks_broken = 0
-#liste des briques
+
+# liste des briques
 bricks_list = []
+
 # variable qui permet la construction des briques une seule fois
 brick_start = True
+
+
 
 
 # variables liées au jeu
@@ -82,6 +95,7 @@ level_1 = True
 level_2 = False
 level_3 = False
 game = True
+
 
 
 
@@ -103,30 +117,39 @@ def platform_move(x, y):
     return x, y
 
 
+
 # fonction qui définit l'accélaration de la balle
 def ball_acceleration(ball_velocity_x, ball_velocity_y, ball_acceleration_x, ball_acceleration_y):
-    if (pyxel.frame_count % 2000 == 0):
-        if ball_acceleration_x > 0:
-            ball_acceleration_x = ball_acceleration_x + 1
-            
-        if ball_acceleration_x < 0:
-            ball_acceleration_x = ball_acceleration_x - 1
-            
-        if ball_acceleration_y > 0:
-            ball_acceleration_y = ball_acceleration_y + 1
-            
-        if ball_acceleration_y < 0:
-            ball_acceleration_y = ball_acceleration_y - 1
-            
+    
+    if ball_run:
+        if (pyxel.frame_count % 2000 == 0):
+            if ball_acceleration_x > 0:
+                ball_acceleration_x = ball_acceleration_x + 1
+                
+            if ball_acceleration_x < 0:
+                ball_acceleration_x = ball_acceleration_x - 1
+                
+            if ball_acceleration_y > 0:
+                ball_acceleration_y = ball_acceleration_y + 1
+                
+            if ball_acceleration_y < 0:
+                ball_acceleration_y = ball_acceleration_y - 1
+                
     return ball_velocity_x, ball_velocity_y, ball_acceleration_x, ball_acceleration_y
 
 
-#fonction qui définit le mouvement de la balle
+
+
+# fonction qui définit le mouvement de la balle
 def ball_move(x, y, a, b, r, health_points, score, ball_run, ball_acceleration_x, ball_acceleration_y, bricks_broken, level_2, level_3):
     
+    # si le joueur à lancé le mouvement de la balle
     if ball_run:
+        # mouvement de la balle
         x = x + a + ball_acceleration_x
         y = y + b + ball_acceleration_y
+        
+        # rebond de la balle sur les briques
         for brick in bricks_list:
         
             if (y + r + b) >= brick[1] and (y - r + b) <= (brick[1] + brick_height) and (x + r + a) >= brick[0] and (x - r + a) <= (brick[0] + brick_width):
@@ -162,7 +185,7 @@ def ball_move(x, y, a, b, r, health_points, score, ball_run, ball_acceleration_x
                     brick[2] = brick_color_2
                 
                 
-            
+        # rebond de la balle sur:    
         # bord droit
         if (x + r + a) >= wall_Right: 
             a = (- a)
@@ -183,15 +206,19 @@ def ball_move(x, y, a, b, r, health_points, score, ball_run, ball_acceleration_x
             ball_run = False
                 
             
-        # rebond plateforme
+        # plateforme
         elif ((y + r + b) > platform_y and (x + a - r) <= (platform_x + platform_width) and (x + a + r) >= platform_x) or ((y + r + b) > platform_y and (x + r + a) < (platform_x) and (x - r + a) > (platform_x - platform_sideWidth) and pyxel.pget((x + a), (y + b)) == platform_color) or ((y + r + b) > platform_y and (x + r + a) < (platform_x + platform_width + platform_sideWidth) and (x - r + a) > (platform_x + platform_width) and pyxel.pget((x + a), (y + b)) == platform_color):
             b = (-b)
             ball_acceleration_y = (- ball_acceleration_y)
-            
+     
+    # revoie les variables  
     return x, y, a, b, r, health_points, score, ball_run, ball_acceleration_x, ball_acceleration_y, bricks_broken, level_2, level_3
 
 
+
+# fonction qui construit les briques en fonction du niveau 1, 2 ou 3
 def bricks_creation(niveau):
+    
     for i in bricks_y:
         for j in bricks_x:
             b_y = i
@@ -213,9 +240,12 @@ def bricks_creation(niveau):
 
 
 
+
 # fonction qui gère les vies
 def health_points_gestion():
+    
     global health_points, ball_y, ball_x, ball_radius, wall_Down, ball_run
+    
     if (ball_y + ball_radius + ball_velocity_y) >= wall_Down:
         ball_run = False
         health_points -= 1
@@ -224,9 +254,9 @@ def health_points_gestion():
 
 
 
-# Update_____________________________________________________________________________________________________
+# Mise à jour 30 fois par second_____________________________________________________________________________________________________
 def update():
-    """mise à jour des variables (30 fois par seconde)"""
+
 
     # appelle les variables et les listes nécessaires
     global health_points, rang, brick_level, ball_totalVelocity, platform_x, platform_y, vies, ball_x, ball_y, ball_velocity_x, ball_velocity_y, ball_radius, b_x, b_y, bricks_list, brick_start, brick_height, brick_width, score, ball_run, ball_acceleration_x, ball_acceleration_y, bricks_broken, bricks_niveau1, bricks_niveau2, level_2, level_3
@@ -236,23 +266,28 @@ def update():
     if pyxel.btnr(pyxel.KEY_SPACE):
         ball_run = True
     
+    
     # si la balle n'est plus en état de se déplacer (touche bord bas)
     if ball_run == False:
         ball_x = screen_width/2
         ball_y = screen_height/2
         
 
-    # mise à jour de la position du vaisseau
+    # déplacement de la plateforme
     platform_x, platform_y = platform_move(platform_x, platform_y)
     
+    
+    # mise à jour de l'acceleration de la balle
     ball_velocity_x, ball_velocity_y, ball_acceleration_x, ball_acceleration_y = ball_acceleration(ball_velocity_x, ball_velocity_y, ball_acceleration_x, ball_acceleration_y)
     
-    #déplacement de la balle
+    
+    # déplacement de la balle
     ball_x, ball_y, ball_velocity_x, ball_velocity_y, ball_radius, health_points, score, ball_run, ball_acceleration_x, ball_acceleration_y, bricks_broken, level_2, level_3 = ball_move(ball_x, ball_y, ball_velocity_x, ball_velocity_y, ball_radius, health_points, score, ball_run, ball_acceleration_x, ball_acceleration_y, bricks_broken, level_2, level_3)
     
     
-    #gestion des vies
+    # gestion des vies
     health_points_gestion()
+    
     
     # si la construction des brique est vrai alors créer les briques 
     if brick_start :
@@ -266,13 +301,17 @@ def update():
 
 # affichage graphique___________________________________________________________________________________________________________________________
 def draw():
+    
     global level_1, level_2, level_3, game
-    """création des objets (30 fois par seconde)"""
+    
+    
     # effacer les éléments de l'écran
     pyxel.cls(0)
 
+
     # si les vies sont supérieures à 0
     if health_points > 0 and game == True:    
+
 
         # platform 
         pyxel.rect(platform_x, platform_y, platform_width, platform_height, platform_color)
@@ -282,14 +321,18 @@ def draw():
         # ball
         pyxel.circ(ball_x, ball_y, ball_radius, ball_color)
         
-        
+        # dessin des briques
         for brick in bricks_list:
             pyxel.rect(brick[0], brick[1], brick_width, brick_height, brick[2])
         
+        # affichage du score
         pyxel.text(10, 10,'Score: %d' % score, 7)
         
+        # affichage des vies
         pyxel.text(360, 10,'vies: %d' % health_points, 7)
         
+        
+        # indique le niveau du jeu 
         if level_1:
             pyxel.text(186,10, 'NIVEAU 1', 7)
             
@@ -313,8 +356,13 @@ def draw():
         if bricks_broken == 63:
             game = False
     
+    
+    
+    # si le jeu est gagné
     elif game == False: 
         pyxel.text(142,150, 'BRAVO TU AS REUSSI LES 3 NIVEAUX', 7)
+        
+        
         
     # si les vies ne sont pas supérieures à 0  
     elif health_points <= 0:
@@ -325,4 +373,3 @@ def draw():
 
 # lancement du jeu _____________________________________________________________________________________________________________________________
 pyxel.run(update, draw)
-
